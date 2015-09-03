2925b82ac069eb2258ea47ef16863a846b7d2e73
Shane Dublin <rushanedublin@gmail.com>
2015-09-02 23:10:11 -0700

    set up a basic main menu

---
+++ Assets/Network/LobbyManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class LobbyManager : NetworkLobbyManager
+{
+
+	static public LobbyManager s_Singleton;
+
+	public GameObject mainPannel;
+	public GameObject hostPannel;
+	public GameObject joinPannel;
+
+	public GameObject currentPanel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		s_Singleton = this;
+		ChangeTo (mainPannel);
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	public override void OnServerSceneChanged (string sceneName)
+	{
+		base.OnServerSceneChanged (sceneName);
+		Debug.Log ("changed");
+	}
+	public override void OnClientSceneChanged (NetworkConnection conn)
+	{
+		base.OnClientSceneChanged (conn);
+		Debug.Log ("client scene changed");
+		ChangeTo (null);
+
+	}
+
+
+	
+	public void ChangeTo (GameObject newPanel)
+	{
+		if (currentPanel != null) {
+			currentPanel.gameObject.SetActive (false);
+		}
+		
+		if (newPanel != null) {
+			newPanel.gameObject.SetActive (true);
+		}
+		
+		currentPanel = newPanel;
+		
+
+	}
+
+}
---
+++ Assets/Network/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	public LobbyManager lobbyManager;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+
+	public void OnClickHost ()
+	{
+		lobbyManager.StartHost ();
+		lobbyManager.showLobbyGUI = true;
+		lobbyManager.ChangeTo (lobbyManager.hostPannel);
+	}
+	
+	public void OnClickJoin ()
+	{
+		//lobbyManager.ChangeTo (lobbyPanel);
+		
+		//lobbyManager.networkAddress = ipInput.text;
+		lobbyManager.networkAddress = "localhost";
+		lobbyManager.StartClient ();
+		lobbyManager.showLobbyGUI = true;
+		lobbyManager.ChangeTo (lobbyManager.joinPannel);
+		
+		//lobbyManager.backDelegate = lobbyManager.StopClientClbk;
+		//	lobbyManager.DisplayIsConnecting ();
+		
+		//lobbyManager.SetServerInfo ("Connecting...", lobbyManager.networkAddress);
+	}
+
+}
